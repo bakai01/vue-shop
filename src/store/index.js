f68ef4eb60666e0278186d269e1e4143cc66428c
bakai <thedesport20@gmail.com>
2021-08-06 12:45:33 +0600

    request is done

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,48 @@
 import { store } from 'quasar/wrappers'
 import { createStore } from 'vuex'
 
-import data from '../api/data.json'
+import dataJson from '../api/data.json'
+import namesJson from '../api/names.json'
 
 export default store(function () {
   const Store = createStore({
     state: {},
     actions: {
       fetchData: () => {
-        console.log(data)
+        const data = JSON.parse(JSON.stringify(dataJson.Value.Goods))
+        const names = JSON.parse(JSON.stringify(namesJson))
+
+        const obj = {}
+        const arr = []
+
+        for (const key in names) {
+          const products = []
+
+          for (const index in names[key].B) {
+            let qop = data.find(product => product.T === +index)
+
+            qop && products.push({
+              id: qop.T,
+              price: qop.C,
+              amount: qop.P,
+              title: names[key].B[index].N,
+            })
+          }
+
+          if (!obj[key] && products.length) {
+            obj[key] = {
+              id: key,
+              categoryTitle: names[key].G,
+              products
+            }
+          }
+        }
+
+        for (const key in obj) {
+          arr.push(obj[key])
+        }
+
+        console.log(arr)
       }
     },
     mutations: {},
